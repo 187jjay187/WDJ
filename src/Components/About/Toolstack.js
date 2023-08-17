c13597159349979024bf5606fecbf82afdff3007
187jjay187 <187jjay187@gmail.com>
2023-08-17 22:16:58 +0200

    Add Toolstack to folder & updated files

---
+++ src/Components/About/Toolstack.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import {
+  SiFigma,
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiGithub,
+  SiAdobe,
+  SiGit,
+  SiRedis,
+  SiPostgresql,
+  SiFirebase,
+  SiMongodb,
+  SiWebpack,
+  SiHeroku,
+  SiNetlify,
+} from 'react-icons/si';
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobe />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGithub />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWebpack />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedis />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSlack />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNetlify />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
