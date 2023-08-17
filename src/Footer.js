2450d7bd84189293037eb5c6d4e0c79ae76cefd9
187jjay187 <187jjay187@gmail.com>
2023-08-17 22:12:18 +0200

    Add Footer section

---
+++ src/Footer.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+
+function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by WebDevJay</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>
+            Copyright ©
+            {year}
+            {' '}
+            WebDevJay
+          </h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/187jjay187"
+                style={{ color: 'white' }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/187jjay187"
+                style={{ color: 'white' }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/jerome-osman"
+                style={{ color: 'white' }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/"
+                style={{ color: 'white' }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
