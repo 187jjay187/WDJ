3789cf1b1c856bf045b135b9c967b9ecfbe96182
187jjay187 <187jjay187@gmail.com>
2023-10-06 23:55:08 +0200

    Add updated tool stack

--- src/Components/About/Toolstack.js
+++ src/Components/About/Toolstack.js
@@ -16,15 +16,6 @@ import {
   SiWebpack,
   SiHeroku,
   SiNetlify,
-  SiAmazonaws,
-  SiAmazons3,
-  SiAmazondynamodb,
-  SiAmazonec2,
-  SiAwsfargate,
-  SiAwslambda,
-  SiAmazonecs,
-  SiAmazoneks,
-  SiAmazonrds,
 } from 'react-icons/si';
 
 function Toolstack() {
@@ -74,33 +65,6 @@ function Toolstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNetlify />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazonaws />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazons3 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazondynamodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazonec2 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAwsfargate />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAwslambda />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazonecs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazoneks />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazonrds />
       </Col>
     </Row>
   );
